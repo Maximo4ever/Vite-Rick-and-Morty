b3cc18f35db8d40d99337a81d8fde7e5eba9ea69
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-25 18:37:39 -0500

    Agrega el listado de los personajes de la API

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CharacterList from "./components/CharacterList";
+
+function App() {
+  return (
+    <main>
+      <h1>Rick and Morty</h1>
+      <CharacterList />
+    </main>
+  );
+}
+
+export default App;
