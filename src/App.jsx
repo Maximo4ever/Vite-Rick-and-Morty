c93c99b6e9fb9dbab66c522b5bbd437c769c4acb
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-25 18:59:11 -0500

    Estiliza el listado con bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import CharacterList from "./components/CharacterList";
 
 function App() {
   return (
-    <main>
-      <h1>Rick and Morty</h1>
+    <main className="bg-dark text-white">
+      <h1 className="text-center display-1 py-3">Rick and Morty</h1>
       <CharacterList />
     </main>
   );
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,21 +3,34 @@ import Character from "./Character";
 
 function CharacterList() {
   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("https://rickandmortyapi.com/api/character");
+      setLoading(true);
+      const response = await fetch(
+        "https://rickandmortyapi.com/api/character?page=2"
+      );
       const data = await response.json();
       setCharacters(data.results);
+      setLoading(false);
     }
     fetchData();
   }, []);
 
   return (
-    <div>
-      {characters.map((character) => (
-        <Character key={character.id} character={character} />
-      ))}
+    <div className="container">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="row">
+          {characters.map((character) => (
+            <div className="col-md-4" key={character.id}>
+              <Character character={character} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
