20858f65b68a76fd1913d72b53440762ccb702c1
Maximo-4ever <maximilianocm06@gmail.com>
2022-06-26 15:11:47 -0500

    Añade la paginacion de los personajes

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -4,22 +4,38 @@ import Character from "./Character";
 function CharacterList() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  function NavPage({ page, setPage }) {
+    return (
+      <header className="d-flex justify-content-between align-items-center mb-3">
+        <p className="mb-0">Page {page}</p>
+        <button
+          onClick={() => setPage(page + 1)}
+          className="btn btn-primary btn-sm"
+        >
+          Page {page + 1}
+        </button>
+      </header>
+    );
+  }
 
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
       const response = await fetch(
-        "https://rickandmortyapi.com/api/character?page=2"
+        `https://rickandmortyapi.com/api/character?page=${page}`
       );
       const data = await response.json();
       setCharacters(data.results);
       setLoading(false);
     }
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
     <div className="container">
+      <NavPage page={page} setPage={setPage} />
       {loading ? (
         <h1>Loading...</h1>
       ) : (
